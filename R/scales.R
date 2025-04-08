63e58433c31988e8f16e1a07e8ff9ee92bc5e524
Christopher Kenny <ctk44@cornell.edu>
2025-04-08 11:58:15 -0400

    implement binned + continuous

--- R/scales.R
+++ R/scales.R
@@ -11,18 +11,18 @@
 #' ggplot2::mpg |>
 #'   ggplot2::ggplot() +
 #'   ggplot2::geom_point(ggplot2::aes(displ, hwy, colour = class)) +
-#'   scale_color_flexoki(palette = 'dark')
+#'   scale_color_flexoki_d(palette = 'dark')
 #'
 #' ggplot2::mpg |>
 #'   ggplot2::ggplot() +
 #'   ggplot2::geom_point(ggplot2::aes(displ, hwy, fill = class),
 #'     pch = 23, color = 'transparent'
 #'   ) +
-#'   scale_fill_flexoki(palette = 'light')
+#'   scale_fill_flexoki_d(palette = 'light')
 #'
 #' @rdname scale_flexoki
 #' @export
-scale_color_flexoki <- function(palette = 'dark', which = NULL, ...,
+scale_color_flexoki_d <- function(palette = 'dark', which = NULL, ...,
                                 reverse = FALSE) {
   pal <- flexoki[[palette]]
   if (!is.null(which)) {
@@ -39,7 +39,7 @@ scale_color_flexoki <- function(palette = 'dark', which = NULL, ...,
 
 #' @rdname scale_flexoki
 #' @export
-scale_fill_flexoki <- function(palette = 'dark', which = NULL, ...,
+scale_fill_flexoki_d <- function(palette = 'dark', which = NULL, ...,
                                reverse = FALSE) {
   pal <- flexoki[[palette]]
   if (!is.null(which)) {
@@ -56,4 +56,59 @@ scale_fill_flexoki <- function(palette = 'dark', which = NULL, ...,
 
 #' @rdname scale_flexoki
 #' @export
-scale_colour_flexoki <- scale_color_flexoki
+scale_colour_flexoki_d <- scale_color_flexoki_d
+
+#' @rdname scale_flexoki
+#' @export
+scale_color_flexoki_c <- function(palette = 'red', ...,
+                                 reverse = FALSE) {
+  pal <- flexoki[[palette]]
+  if (reverse) {
+    pal <- rev(pal)
+  }
+  ggplot2::scale_colour_gradientn(..., colours = pal)
+}
+
+#' @rdname scale_flexoki
+#' @export
+scale_fill_flexoki_c <- function(palette = 'red', ...,
+                                 reverse = FALSE) {
+  pal <- flexoki[[palette]]
+  if (reverse) {
+    pal <- rev(pal)
+  }
+  ggplot2::scale_fill_gradientn(..., colours = pal)
+}
+
+#' @rdname scale_flexoki
+#' @export
+scale_colour_flexoki_c <- scale_color_flexoki_c
+
+
+#' @rdname scale_flexoki
+#' @export
+scale_color_flexoki_b <- function(palette = 'red', ...,
+                                  reverse = FALSE) {
+  pal <- flexoki[[palette]]
+  if (reverse) {
+    pal <- rev(pal)
+  }
+  ramp <- scales::colour_ramp(pal)
+  ggplot2::binned_scale('color', palette, palette = ramp, ...)
+}
+
+#' @rdname scale_flexoki
+#' @export
+scale_fill_flexoki_b <- function(palette = 'red', ...,
+                                 reverse = FALSE) {
+  pal <- flexoki[[palette]]
+  if (reverse) {
+    pal <- rev(pal)
+  }
+  ramp <- scales::colour_ramp(pal)
+  ggplot2::binned_scale('fill', palette, palette = ramp, ...)
+}
+
+#' @rdname scale_flexoki
+#' @export
+scale_colour_flexoki_b <- scale_color_flexoki_b
